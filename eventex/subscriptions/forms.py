5ab6f0fa31d0754935fc5f83f08e2bf7420d2f49
Geovane Carvalho <geovaneoliveira.75@hotmail.com>
2020-09-26 00:36:58 -0300

    Simplifica com modelform

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from eventex.subscriptions.models import Subscription 
 
 def validate_cpf(value):
 	if not value.isdigit():
@@ -9,7 +10,7 @@ def validate_cpf(value):
 		raise ValidationError('CPF deve ter 11 números.')
 
 
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
 	name = forms.CharField(label='Nome')
 	cpf = forms.CharField(label="CPF", validators=[validate_cpf])
 	email = forms.EmailField(label="E-mail" , required=False)
@@ -23,6 +24,26 @@ class SubscriptionForm(forms.Form):
 		return " ".join(words)
 
 	def clean(self):
+		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
+			raise ValidationError('Informe seu E-mail ou Telefone.')
+		return self.cleaned_data
+
+
+class SubscriptionForm(forms.ModelForm):
+	class Meta:
+		model = Subscription
+		fields = ['name', 'cpf', 'email','phone']
+
+
+	def clean_name(self):
+		name = self.cleaned_data['name']
+		
+		words = [w.capitalize() for w in name.split()]
+		return " ".join(words)
+
+	def clean(self):
+		self.cleaned_data = super().clean()
+		
 		if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
 			raise ValidationError('Informe seu E-mail ou Telefone.')
 		return self.cleaned_data
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -25,8 +25,8 @@ def create(request):
 	if not form.is_valid():
 		return render(request, 'subscriptions/subscription_form.html', {'form': form})
 
+	subscription = form.save()
 	
-	subscription = Subscription.objects.create(**form.cleaned_data)
 	# Send email
 	_send_mail('Confirmação de inscrição',
 			   settings.DEFAULT_FROM_EMAIL,
