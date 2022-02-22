1c9894137b1ef67dfad615336f5808ee27418db6
geovanecarvalho <geovaneoliveira.75@hotmail.com>
2022-02-22 19:43:37 -0300

    config sendgrid'

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -18,7 +18,7 @@ def subscribe(request):
 
             mail.send_mail('Confirmação de inscrição',
                             body,
-                            'contato@eventex.com.br',
+                            'geovanehacker.io@gmail.com',
                             ['contato@eventex.com.br', form.cleaned_data['email']])
             messages.success(request, 'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
