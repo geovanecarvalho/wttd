da7317b83a52a76afacbd5b4c38f20805a6b1175
geovanecarvalho <geovaneoliveira.75@hotmail.com>
2022-02-25 01:40:52 -0300

    Extrai urls hardcoded

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -2,10 +2,11 @@ from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 from django.core import mail
 from eventex.subscriptions.models import Subscription
+from django.shortcuts import resolve_url as r
 
-class SubscribeGet(TestCase):
+class SubscribeNewGet(TestCase):
     def setUp(self):
-        self.resp = self.client.get('/inscricao/')
+        self.resp = self.client.get(r('subscriptions:new'))
 
     def test_get(self):
         """GET /inscricao/ must return status code 200."""
@@ -41,16 +42,16 @@ class SubscribeGet(TestCase):
     
 
 
-class SubscribePostValid(TestCase):
+class SubscribeNewPostValid(TestCase):
     def setUp(self):
         data = dict(name='Henrique Bastos', cpf='12345678901', 
         email='henrique@bastos.net', phone='21-99618-6180')
 
-        self.resp = self.client.post('/inscricao/', data)
+        self.resp = self.client.post(r('subscriptions:new'), data)
 
     def test_post(self):
         """Valid POST should redirect to /inscricao/1/."""
-        self.assertRedirects(self.resp, '/inscricao/1/')
+        self.assertRedirects(self.resp, r('subscriptions:detail', 1))
     
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -60,9 +61,9 @@ class SubscribePostValid(TestCase):
 
     
 
-class SubscribePostInvalid(TestCase):
+class SubscribeNewPostInvalid(TestCase):
     def setUp(self):
-        self.resp = self.client.post('/inscricao/', {})
+        self.resp = self.client.post(r('subscriptions:new'), {})
     def test_post(self):
         """Invalid POST should not redirect"""
         
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,14 +5,17 @@ from django.core import mail
 from django.template.loader import render_to_string
 from django.conf import settings
 from eventex.subscriptions.models import Subscription
+from django.shortcuts import resolve_url as r
 
-def subscribe(request):
+def new(request):
     if request.method == 'POST':
         return create(request)
-    else:
-        return new(request)
+    
+    return empty_form(request)
     
    
+def empty_form(request):
+    return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
 
 
 def create(request):
@@ -30,13 +33,9 @@ def create(request):
                  'subscriptions/subscription_email.txt',
                  {'subscription': subscription})
     
-    return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
+    return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
         
     
-
-def new(request):
-    
-    return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
 
 def detail(request, pk):
     try:
