76e97ca9998ae1fdcc057f12e1c9465fb0ab57d3
geovanecarvalho <geovaneoliveira.75@hotmail.com>
2022-03-08 01:40:45 -0300

    Courses

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from eventex.core.models import Talk
+from eventex.core.models import Talk, Course
 from eventex.core.manage import PeriodManager
 
 
@@ -62,3 +62,31 @@ class PeriodManagerTest(TestCase):
         expected = ['Afternoon Talk']
 
         self.assertQuerysetEqual(qs, expected, lambda o:o.title)
+
+
+class CourseModelTest(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title='Título do Curso',
+            start='09:00',
+            description='Descrição do curso.',
+            slots=20
+        )
+
+    def test_create(self):
+        self.assertTrue(Course.objects.exists())
+
+    def test_speaker(self):
+        self.course.speakers.create(
+            name='Henrique Bastos',
+            slug='henrique-bastos',
+            website='http://henriquebastos.net'
+        )
+        self.assertEqual(1, self.course.speakers.count())
+
+    def test_str(self):
+        self.assertEqual('Título do Curso', str(self.course))
+
+    
+    def test_manager(self):
+        self.assertIsInstance(Course.objects, PeriodManager)
--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from eventex.core.models import Speaker, Talk
+from eventex.core.models import Speaker, Talk, Course
 
 
 
@@ -14,8 +14,10 @@ def speaker_detail(request, slug):
 
 
 def talk_list(request):
+    
     context = {
         'morning_talks': Talk.objects.at_morning(),
         'afternoon_talks': Talk.objects.at_afternoon(),
+        'courses': Course.objects.all(),
     }
     return render(request, 'core/talk_list.html', context)
